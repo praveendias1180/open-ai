e0bc352d9bd84544cee6221fcb0f72a503f1b3b0
Praveen Dias <praveendias1180@gmail.com>
2022-12-25 11:56:25 +0530

    typeText and generateUniqueId functions

--- client/script.js
+++ client/script.js
@@ -16,3 +16,23 @@ function loader(element){
     }
   }, 300)
 }
+
+function typeText(element, text){
+  let index = 0;
+  
+  let interval = setInterval(() => {
+    if(index < text.length){
+      element.innerHTML  += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+function generateUniqueId(){
+  const timestamp = Date.now();
+  const randomNumber = Math.random();
+  const haxadecimalString = randomNumber.toString(16);
+  return `id-${timestamp}-${haxadecimalString}`;
+}
